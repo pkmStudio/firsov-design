875ab89166cfb23758e60f66876e2a02b7e73770
арсений <rodivilov-indastriz@yandex.ru>
2025-05-07 12:36:02 +0300

    pixel perfect mobile menu

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,6 @@
 
+
+
 document.querySelectorAll('textarea').forEach(textarea => {
     console.log(textarea);
     textarea.addEventListener('input', function() {
@@ -7,9 +9,33 @@ document.querySelectorAll('textarea').forEach(textarea => {
 });
     
 
+   // Функция для бургер-меню (если ещё не добавлена)
+   document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const menuLinks = mobileMenu.querySelectorAll('a');
+    console.log(burger);
+    
+  
+    function toggleMenu() {
+      burger.classList.toggle('open');
+      mobileMenu.classList.toggle('open');
+      document.body.style.overflow = mobileMenu.classList.contains('open') ? 'hidden' : '';
+    }
+  
+    burger.addEventListener('click' , toggleMenu);
+  
+    menuLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        burger.classList.remove('open');
+        mobileMenu.classList.remove('open');
+        document.body.style.overflow = '';
+      });
+    });
+  });
+
     document.addEventListener('DOMContentLoaded', function() {
         const header = document.querySelector('.header');
-        
         window.addEventListener('scroll', function() {
             if (window.scrollY > 10) { // Если прокрутили больше 10px
                 header.classList.add('scrolled');
@@ -19,8 +45,4 @@ document.querySelectorAll('textarea').forEach(textarea => {
         });
     });
 
-    // Функция для бургер-меню (если ещё не добавлена)
-    function toggleMenu() {
-        const mobileMenu = document.querySelector('.mobile-menu');
-        mobileMenu.classList.toggle('open');
-    }
+ 
